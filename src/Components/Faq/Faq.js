0e02de071ffa133c5bdea929eb515f20c35ecddf
sazzad <sazadssd612@gmail.com>
2024-06-05 12:49:26 +0600

    fixed_faq_section

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -1,5 +1,5 @@
 "use client";
-import { readexPro } from "@/utils/Font";
+import { nunito, readexPro } from "@/utils/Font";
 import React, { useState } from "react";
 import minus from "../../../public/assets/minus.png";
 import plus from "../../../public/assets/plus.png";
@@ -54,42 +54,52 @@ const Faq = () => {
         "Vitae scelerisque nec nunc eget. Feugiat eget ut venenatis amet vulputate amet urna ut. Risus viverra ac risus vulputate sit amet parturien.",
     },
   ];
+
   return (
     <div>
       <h1
-        className={` text-[50px] font-bold ${readexPro.className} text-[#333333] text-center pb-[10px]`}
+        className={`text-[50px] font-bold ${readexPro.className} text-[#333333] text-center pb-[10px] transition-all duration-300 ease-in-out`}
       >
         Have a question?
       </h1>
-      <div className=" grid lg:grid-cols-2 grid-cols-1 gap-x-[36px] gap-y-[24px] ">
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-[36px] gap-y-[24px]">
         {faqData.map((item, index) => (
           <div
             key={index}
-            className={`border border-gray-300 p-4 rounded   ${
-              openIndex == index ? "backgroundColor" : "bg-[#FAFAFC]"
-            }  border-none`}
+            className={`border p-4 rounded transition-all duration-300 ease-in-out ${
+              openIndex === index
+                ? "backgroundColor text-white"
+                : "bg-[#FAFAFC]"
+            }`}
           >
-            <div className="cursor-pointer flex justify-between items-center">
+            <div
+              className="cursor-pointer flex justify-between items-center"
+              onClick={() => handleClick(index)}
+            >
               <h2
-                className={`text-lg font-semibold ${
-                  openIndex == index && "text-white"
+                className={`text-[18px] font-semibold ${readexPro.className} ${
+                  openIndex == index ? "text-white" : "text-[#333333]"
                 }`}
               >
                 {item.question}
               </h2>
-              <span onClick={() => handleClick(index)}>
+              <span>
                 {openIndex === index ? (
-                  <Image src={minus} alt="minus" />
+                  <Image src={minus} alt="minus" width={24} height={24} />
                 ) : (
-                  <Image src={plus} alt="plus" />
+                  <Image src={plus} alt="plus" width={24} height={24} />
                 )}
               </span>
             </div>
-            {openIndex === index && (
-              <p className={` ${openIndex == index && "text-white"}`}>
-                {item.answer}
-              </p>
-            )}
+            <div
+              className={`overflow-hidden transition-all duration-500 ease-in-out  ${
+                openIndex === index
+                  ? `max-h-96 opacity-100  2xl:w-[520px] xl:w-[520px] lg:w-[520px] w-full pt-[10px] ${nunito.className} font-medium text-[16px]`
+                  : "max-h-0 opacity-0"
+              }`}
+            >
+              <p className="mt-2">{item.answer}</p>
+            </div>
           </div>
         ))}
       </div>
